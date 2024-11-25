3dbe5a296848a33f70e037612b849b98189e8c6b
Kendrick Halder <108589219+halderkendrick@users.noreply.github.com>
2024-11-25 10:55:37 +0600

    Add files via upload

---
+++ problem.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int num = 20;
+    printf("Integer is %d", num);
+    float avg = 52.54;
+    printf("Floating point number is %f", avg);
+    char x = 'A';
+    printf("Character is %c", x);
+    char y = '29';
+    printf("Character is %c", y);
+}
